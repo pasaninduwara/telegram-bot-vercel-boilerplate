f6ef4e22035518583d1cedce7da420ecb3574d0b
pasaninduwara <pasanxinduwara@gmail.com>
2026-02-26 01:34:12 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,16 @@ bot.command('about', about());
 // 2. NEW: Only welcome new members
 bot.on('new_chat_members', async (ctx) => {
     const newUser = ctx.message.new_chat_members[0];
-    await ctx.reply(`Welcome to the group, ${newUser.first_name}!`);
+    const name = newUser.first_name;
+
+    const welcomeMessage = 
+`Hello ${name}, Welcome to Cosmonauts by Roscosmos. 
+
+Here we explore Aliens, UFOs, Space, Underwater mysteries and unexplained phenomena that refuse to be explained. we explore it all together. 👽❤
+
+> Look, @Awoks_x2 ! Our Roscosmos Team just gained a new space buddy.`;
+
+    await ctx.replyWithMarkdownV2(welcomeMessage.replace(/([_*\[\]()~`>#+\-=|{}.!])/g, '\\$1'));
 });
 
 // 3. REMOVED: bot.on('message', greeting()) is gone so it stays silent during chat
